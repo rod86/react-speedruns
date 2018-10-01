dbc688ca7834188c14a6591ae354a345099a9121
Sergi Rodriguez <>
2018-10-01 15:28:32 +0200

    no message

--- src/components/GamesList/GameItem.js
+++ src/components/GamesList/GameItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const GameItem = ({ game }) => {
     return (
@@ -8,7 +9,7 @@ const GameItem = ({ game }) => {
             <div className="media-body">
                 <h5 className="mt-0 mb-1">{game.names.international}</h5>
                 <div className="text-right">
-                    <a href={`/game/${game.id}`} className="btn btn-primary">View</a>
+                    <Link to={`/game/${game.id}`} className="btn btn-primary">View</Link>
                 </div>
             </div>
         </li>
